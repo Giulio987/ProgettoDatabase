156388ecd8798c08aa7f8fc8943b870814fad6ce
zeroCool <giulio.milani99@gmail.com>
2020-05-16 10:21:08 +0200

    Prove iniziali con commit

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "Ciao sono Giulio";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Prova Titolo Pagina </title>
+    </head>
+    <body>
+        <h1> Prova Intestazione</h1>
+        <ol>
+            <li>Elemento 1 </li>
+            <li>Elemento 2 </li>
+            <li>Elemento 3 </li>
+        </ol>
+<?PHP echo "Prova php"; ?>
+    </body>
+</html>
