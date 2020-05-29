25713a11eb219a0b02807880babd065d400f8893
zeroCool <giulio.milani99@gmail.com>
2020-05-29 11:53:13 +0200

    aggiunta Insert.sql

---
+++ INSERT.sql
@@ -0,0 +1,29 @@
+USE Biblioteca;
+#OGNI DIPARTIMENTO HA LA PROPRIA BIBLIOTECA LO DICE IL TESTO
+INSERT INTO DIPARTIMENTO VALUES('Architettura','Via Giara','36','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Economia e management','Via Voltapaletto','11','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Fisica e Scienze della Terra','Via Saragat','1','44122','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Giurisprudenza','Corso Ercole I d\'Este','37','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Ingegneria','Via Saragat','1','44122','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Matematica e Informatica','Via Macchiavelli','30','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Morfologia, chirurgia e medicina sperimentale','Via Luigi Borsari','46','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Scienze biomediche e chirurgico specialistiche','Via Luigi Borsari','46','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Scienze chimiche e farmaceutiche','Via Luigi Borsari','46','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Scienze della vita e biotecnologie','Via Luigi Borsari','46','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Scienze mediche','Via Fossato di Mortara','64/B','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Studi umanistici','Via LParadiso','12','44121','Ferrara');
+INSERT INTO DIPARTIMENTO VALUES('Facoltà di Medicina, Farmacia e Prevenzione', 'Via Fossato di Mortara','70','44121','Ferrara');
+
+
+INSERT INTO EDITORE VALUES(1,'Apogeo','02-3596681','Via Andegari','6','20121','Milano');
+INSERT INTO EDITORE VALUES(2,'Pearson Italia','011-75021510','Corso Trapani','16','10139','Torino');
+INSERT INTO EDITORE VALUES(3,'Tecniche Nuove','02-390901','Via Eritrea','21','20157','Milano');
+INSERT INTO EDITORE VALUES(4,'Packt Publishing','903-259-1261', 'Tennessee Circle','4','89654','Boston');
+INSERT INTO EDITORE VALUES(5,'Addison-Wesley','638-435-4004','Coolidge Junction','12','7030','Oboken');
+INSERT INTO EDITORE VALUES(6,'Pst Edizioni','02-546722','Via degli Editori Riuniti','8','20157','Milano');
+INSERT INTO EDITORE VALUES(7,'Dunning Editions','16-339-1269','Dunning Drive','988','23445','Lawrance');
+INSERT INTO EDITORE VALUES(8,'Europa Edizioni','02-835610','Via degli Editori Riuniti','34','20157','Milano');
+INSERT INTO EDITORE VALUES(9,'Plaza Publicacionès','03-567234','Strada Grande','944','2264','Madrid');
+INSERT INTO EDITORE VALUES(10,'Meaning','309-306-5066','Lakewood Gardens Place','8734','28063','New York');
+
+#SI AMMETTE CHE IL DB DELL'Università abbia gia I dati di tutti gli studenti anche se non hanno mai effettuato un prestito
